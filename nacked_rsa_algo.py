f7938a9f55794d55625e6c362987ced5e713e891
Fernando Vera <102202733+fernandoveragul@users.noreply.github.com>
2022-11-25 09:51:08 +0500

    Update nacked_rsa_algo.py

--- nacked_rsa_algo.py
+++ nacked_rsa_algo.py
@@ -1,6 +1,8 @@
 class RSA:
     @classmethod
     def __is_prime(cls, number: int) -> bool:
+        """This function check : number is prime?"""
+        
         i = 1
         a = []
         while i ** 2 <= number:
@@ -20,41 +22,41 @@ class RSA:
         
         fi = (p - 1) * (q - 1)
         exp = [i for i in range(fi) if cls.__is_prime(i) and fi % i != 0]
-        
-        mod = (lambda p, q: p * q if cls.__is_prime(p) and cls.__is_prime(q) else None)(p, q)
-        
-        e = exp[0] if exp[0] != mod else exp[-1]
+        mod = (lambda p, q: p * q if all(list(map(cls.__is_prime, [p, q]))) else None)(p, q)
+        e = exp[-1]
         return (e, mod, fi) if mod else None
         
     @classmethod
     def __gen_keys(cls, p: int, q: int) -> tuple:
+        """This function generate and return (public_key, private_key)"""
+        
         e, mod, fi = cls.__func_eler(p, q)
-        d : int = 0
+        d: int = 0
         while (d * e) % fi != 1: 
             d += 1
             if d == e:
                 d += 1
-        
         public_key = (e, mod)
         private_key = (d, mod)
-        print(e, fi, mod, d)
-        
         return public_key, private_key
         
     @classmethod
-    def encode(cls, *, message: int, p_q: list):
-        public_key, _ = cls.__gen_keys(*p_q)
-        e, mod = public_key
-        if message < mod:
-            msg = message ** e
-            return msg % mod
+    def encode(cls, *, message: int, p_q: list) -> int:
+        if public_key := cls.__gen_keys(*p_q)[0]:
+            e, mod = public_key 
+            if message < mod:
+                return (message ** e) % mod
+            else:
+                raise ValueError('[!] Mesasge is very long')
         else:
-            raise ValueError('[!] Mesasge is very long')
-    
-    @classmethod
-    def decode(cls, *, msg: int, p_q: list):
-        _, private_key = cls.__gen_keys(*p_q)
-        d, mod = private_key
-        message = msg ** d
-        return message % mod
+            raise ValueError('[!] Public key is empty!')
         
+    @classmethod
+    def decode(cls, *, message: int, p_q: list) -> int:
+        if private_key := cls.__gen_keys(*p_q)[-1]:
+            d, mod = private_key
+            print(message ** d)
+            return (message ** d) % mod
+        else:
+            raise ValueError('[!] Private key is empty!')
+            
